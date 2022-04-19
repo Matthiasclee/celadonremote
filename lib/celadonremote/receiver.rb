08f6714e8ddbe32ea3263996523bea7bf8a25d00
Matthiasclee <matthias@matthiasclee.com>
2022-04-18 18:25:08 -0700

    Added hold amount

--- lib/celadonremote/receiver.rb
+++ lib/celadonremote/receiver.rb
@@ -15,20 +15,24 @@ module CeladonRemote
     def start_listener(allow_hold:false)
       prev_line = ""
 
+      h = 0
+
       loop do
         l=@ser.readline(@eol)
 
         if allow_hold && l.start_with?(@eot)
           code = prev_line[@code]
+          h = h+1
         elsif l.start_with?(@eot)
           next
         else
           code = l[@code]
+          h = 0
           prev_line = l
         end
 
         if @codes[code]
-          @codes[code].call
+          @codes[code].call(h)
         end
       end
     end
